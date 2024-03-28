00eb431537f8e9419823dcb7235a473394a20bec
ryanrare <ryan.hollmes@gmail.com>
2024-03-28 16:30:54 -0300

    erro de linha

--- controlers/quadro_trabalho.js
+++ controlers/quadro_trabalho.js
@@ -1,6 +1,11 @@
+const fs = require("fs")
+
+
 function getQuadroTrabalho(req, res) {
     try {
-        res.send('lista de quadro de trabalho!')
+        const livros = JSON.parse(fs.readFileSync("quadro_trabalho.json"))
+
+        res.send(livros)
     } catch (error) {
         res.status(500)
         res.send(error.message)
